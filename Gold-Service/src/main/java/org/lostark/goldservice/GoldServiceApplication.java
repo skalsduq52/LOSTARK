749b0d0ee4713492123a9986bc2a69b45b36139b
skalsduq52 <skalsduq@naver.com>
2025-01-16 17:31:18 +0900

    init Commit

---
+++ Gold-Service/src/main/java/org/lostark/goldservice/GoldServiceApplication.java
@@ -0,0 +1,13 @@
+package org.lostark.goldservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldServiceApplication.class, args);
+	}
+
+}
---
+++ Gold-Service/src/test/java/org/lostark/goldservice/GoldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lostark.goldservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
